fc61f421ea647a8f183f8a2c6468c959f4537594
Ethan Lacey <ethanlacey@ESA-4.local>
2024-09-26 16:42:15 +0100

    Created four different types of variables.

---
+++ variables.py
@@ -0,0 +1,6 @@
+name = "Ethan"                      # String
+age = 12                            # Integer
+height = 1.70                       # Float
+isCold = False                      # Boolean
+
+print("My name is", name, "I am", age, "years old", "I am", height, "metres tall", "Am I cold?", isCold)
